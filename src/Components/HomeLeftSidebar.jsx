0dc13a34dd93ff3405e56552135f1243ce6669e1
SAURABH-UPRETI <saurabh.urt@gmail.com>
2023-07-27 21:56:36 +0530

    first commit

---
+++ src/Components/HomeLeftSidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./HomeLeftSidebar.css";
+
+const HomeLeftSidebar = () => {
+  return (
+    <div>HomeLeftSidebar</div>
+  )
+}
+
+export default HomeLeftSidebar
---
+++ src/Components/HomeRightSidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./HomeRightSidebar.css"
+
+const HomeRightSidebar = () => {
+  return (
+    <div>HomeRightSidebar</div>
+  )
+}
+
+export default HomeRightSidebar
